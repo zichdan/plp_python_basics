0e48e1cdfe788533432969f9a7e9ff17ac3e1640
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-02-23 21:28:54 +0100

    "THe initial commit"
    "first commit"

---
+++ list_pract.py
@@ -0,0 +1,70 @@
+# my_list = [] #empty list
+
+# list_a = [1,2,3,4,5] #int
+
+# list_b = ["blessing", "daniel", "ezichi", "zichdan"]  # string
+
+# list_c = [1,"skinny", "daniel", True, 12.59]  # list items of different types
+
+# list_d = [1,2, [3,4],5] # Nested List
+
+# str_var = "daniel"
+
+
+# print (type(list_a))
+# # print (type(list_b))
+# # print (type(list_c))
+# # print (type(list_d))
+
+
+# student = []
+
+# student.append("Bello")
+# student.append("Bobby")
+# student.append("Trent")
+# student.append("Virgil")
+
+# # print(student)
+
+languages = [ "python", 'ruby',"swift", 'c', "java", "Rust", "R" ]
+
+#####to check vulnerability in a ist
+# print('d' in languages)
+# print('python' in languages)
+
+
+
+
+# del languages[-1]
+# print(languages)
+
+# # #to delete the first two elemnts
+# # del languages[0:2]
+# # print(languages)
+
+
+# languages.append ("new item")
+
+# print ( languages)
+
+
+
+# prime_numbers = [2,3,5]
+
+# even_numbers = [4,6,6]
+
+# prime_numbers.append(even_numbers)
+# print('list append is:', prime_numbers)
+
+# prime_numbers.extend(even_numbers)
+# print('list extend is:', prime_numbers)
+
+##### LOOPS
+
+
+
+
+
+
+
+
---
+++ tuple_pract.py
@@ -0,0 +1,7 @@
+my_tuple = ('a','p','p','l','e')
+
+#Nested tuple
+# my_tuple = ('mouse', [8,4,6], (1,2,3))
+print(my_tuple.count('p'))
+
+
